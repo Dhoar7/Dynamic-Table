def88e51b6126eeb70c53cbe607e1b5b53973749
Arun Kumar <marun3400@gmail.com>
2022-01-11 01:15:44 +0530

    First Push Dynamic Table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as ReactBootStrap from "react-bootstrap"
 
-function App() {
+const  App = ()=>{
+
+  const mainData = [
+    {position:"LW", name: "Arun", score: '3'},
+    {position:"CM", name: "Sab", score: '2'},
+    {position:"ST", name: "Nare", score: '5'},
+    {position:"RW", name: "Nish", score: '1'}
+  ];
+
+  const head = mainData.map(e=>Object.keys(e))[0];
+  const data = mainData.map(e=>Object.values(e));
+
+
+  const renderPlayer = (dat, index)=>{
+    return(
+      <tr key={index}>
+        {dat.map((e, i)=><td key={i}>{e}</td>)}
+      </tr>
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <ReactBootStrap.Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+    {head.map((e, i)=>{
+    return <th key={i}>{e}</th>
+    })}
+    </tr>
+  </thead>
+  <tbody>
+    {data.map(renderPlayer)}
+  </tbody>
+</ReactBootStrap.Table>
+</div>
   );
 }
 
